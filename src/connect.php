88cda3a2a1a4d07b1c5cd1070d7c8e41b1555ae2
Misbahudin <misbahudin.id@gmail.com>
2024-04-11 23:04:56 +0700

    fix: query insert

--- src/connect.php
+++ src/connect.php
@@ -1,20 +1,26 @@
 <?php
-$servername = "localhost";
-$username = "username";
+$host = "localhost";
+$username = "root";
 $password = "";
 $dbname = "mahasiswa";
+$port = 3306;
 
 try {
-  $conn = new PDO("mysql:host=localhost;dbname=mahasiswa", "username", "");
+  $conn = new PDO("mysql:host=$host:$port;dbname=$dbname", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  $sql = "INSERT INTO `akademik` (`id`, `NAME`, `DATE`, `PRODI`, `NIM`) VALUES (NULL, '', '', '', '')";
-  // use exec() because no results are returned
-  $conn->exec($sql);
-  echo "New record created successfully";
-} catch(PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
-}
 
+  $sql = "INSERT INTO `akademik` (`name`, `prodi`, `nim`) VALUES ('Mizz', 'TI', '123')";
+  // untuk mengeksekusi query
+  $result = $conn->exec($sql);
+
+  if ($result) {
+    echo "New record created successfully";
+  } else {
+    echo "failed insert to db";
+  }
+} catch (PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
+// tutup koneksi
 $conn = null;
-?>
